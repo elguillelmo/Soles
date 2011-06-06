edfc4898d6c81ba217344342e551f23529fa177b
Guillermo Garrido <guille@guille-desktop.(none)>
2011-06-06 19:59:39 +0200

    Changed License to GPL. Several minor changes.

---
+++ make.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p -v ./web/lib
+mkdir -p -v ./web/data
+cp -v ./lib/arbor-graphics.js  ./lib/arbor.js  ./lib/arbor-tween.js  ./lib/graphics.js  ./lib/jquery.1.4.4.min.js  ./lib/jquery.address-1.3.1.min.js ./web/lib/
+cp -v ./sol.js ./index.html ./style.css ./web
+python ./generate_json.py -i ./data/grupos.txt -o ./web/data/grupos.json
+
